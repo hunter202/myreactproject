e4a7351b5b8ddca1e218d24ba23922f3dde82345
hunter202 <swastik.mittaloff@gmail.com>
2016-09-20 22:40:30 +0530

    second commit

--- App.jsx
+++ App.jsx
@@ -1,13 +1,35 @@
 import React from 'react';
 
 class App extends React.Component {
-   render() {
+   render() {            //render is used to process and display html
+   	  var mystyle={      
+       color:"red",
+       fontSize:60
+   	  }
+      var value=1;
       return (
          <div>
-            Hello World!!!
+            <p>hello lets begin!!</p>          
+            {/*what is the use of attribute data-**/}
+            <p data-value="answer">{1+1}</p>   
+            {/*{} are used to process javascript in html*/}
+            <p style={mystyle}>{value==1?'if yes':'if no'}</p>
+            <Test /> 
          </div>
       );
    }
 }
+
+class Test extends React.Component{
+     render() {
+     	return(
+     		  <div>
+     		      <p>wooh... other component</p>
+     		  </div>
+     		  );
+     }
+
+}
+
 
 export default App;
